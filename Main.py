d6397511f33d5d8871e0c0501724e80273369b5c
AndreasAAR <Andreas.ahrlund@gmail.com>
2020-01-27 14:14:56 +0100

    first

---
+++ Main.py
@@ -0,0 +1,8 @@
+'''
+Auth: Andreas Ährlund-Richter
+Using algorithm theorized to solve code-test at Multisoft
+proposed at Codeproject.com by Richard Deeming
+'''
+
+#Calc number of rows in the database "Vacation" COUNT(*), its "C" in the formula in " The Tests and Background "
+
